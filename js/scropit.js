21becce54c9c05ae22907ae82899b56d403a856e
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-14 15:25:03 +0100

    esercizio finito

--- js/scropit.js
+++ js/scropit.js
@@ -7,7 +7,7 @@ for (let i = 1; i <= 100; i ++){
         }else if( i % 5 == 0){
             console.log("Multiplo di 5")
 
-        }else if( i % 3 && % 5){
+        }else if( i % 3 &&  5){
             console.log("Multiplo di 3 e di 5")
         }
         
