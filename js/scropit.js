2bd9fab44106b30c01312e58f467225369170676
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-14 14:33:44 +0100

    scritti i multipli di 3 e quelli di 5

---
+++ js/scropit.js
@@ -0,0 +1,12 @@
+for (let i = 1; i <= 100; i ++){
+    console.log (i)
+
+    if( i % 3 == 0 ) { 
+        console.log("Multiplo  di 3");  
+
+        }else if( i % 5 == 0){
+            console.log("Multiplo di 5")
+
+        }
+        
+}
