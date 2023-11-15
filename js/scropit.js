bc8ce7f0dbd3241ffa08cf6b5846195d1598123e
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-15 10:46:58 +0100

    es. completato correttamente con modifiche

--- js/scropit.js
+++ js/scropit.js
@@ -1,14 +1,16 @@
 for (let i = 1; i <= 100; i ++){
     console.log (i)
 
-    if( i % 3 == 0 ) { 
-        console.log("Multiplo  di 3");  
+    if( i % 3 == 0 && i % 5 == 0){ 
+        console.log("FizzBuzz");  
+
+        }else if( i % 3 == 0){
+            console.log("Fizz")
 
         }else if( i % 5 == 0){
-            console.log("Multiplo di 5")
-
-        }else if( i % 3 &&  5){
-            console.log("Multiplo di 3 e di 5")
+            console.log("Buzz")
+        }else {
+            console.log(i)
         }
         
 }
